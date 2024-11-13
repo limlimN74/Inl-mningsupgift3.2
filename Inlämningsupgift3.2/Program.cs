e6c12b0144401ee576cb7a014837fb993121bdfd
lini07003 <lini07003@E-PW05C37N>
2024-11-13 12:15:09 +0100

    Add project files.

---
+++ Inlämningsupgift3.2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace InlämningsUpgift_3._2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Har du gått ut gymnasiet? (j/n): ");
+            char Gåttut = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
+            if (Gåttut == 'j' || Gåttut == 'J')
+            {
+                Console.Write("Hur gammal är du? ");
+                int år = int.Parse(Console.ReadLine());
+
+                if (år < 22)
+                {
+                    Console.WriteLine("Vi vill gärna anställa dig");
+                }
+                else
+                {
+                    Console.WriteLine("Vi letar tyvärr efter annan personal just nu");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Vi letar tyvärr efter annan personal just nu");
+            }
+        }
+    }
+}
